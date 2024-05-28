53feff33878e9e12ca3fbe396fe93eab3926845c
Carolina Astolfo <156844626+CarolinaAstolfoSectionC@users.noreply.github.com>
2024-05-28 14:48:54 -0400

    no pc agr

---
+++ my-app/next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'png.pngtree.com',
+          port: '',
+          pathname: '/png-vector/20220416/ourmid/pngtree-cookies-cartoon-vector-illustration-png-image_4545491.png',
+        },
+      ],
+    },
+  }
